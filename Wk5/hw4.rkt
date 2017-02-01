88fca2665cebf6f8f10747251848eb3e14bca172
Brent Sullivan <brntsllvn@gmail.com>
2017-01-31 21:44:48 -0800

    p2, p3

--- Wk5/hw4.rkt
+++ Wk5/hw4.rkt
@@ -1,9 +1,22 @@
 
 #lang racket
 
-(provide (all-defined-out)) ;; so we can put tests in a second file
+(provide (all-defined-out)) 
 
-;; put your code below
-
+; #1
 (define (sequence low high stride)
-  1)
+  (cond [(= low high) (cons low null) ]
+        [(> low high) null]
+        [#t (cons low (sequence (+ low stride) high stride))]))
+
+; #2
+(define (string-append-map xs suffix)
+  (cond [(null? xs) (cons suffix null)]
+        [#t (map (lambda(str)(string-append str suffix)) xs)]))
+
+; #3
+(define (list-nth-mod xs n)
+  (cond [(<= n -1)(error "list-nth-mod: negative number")]
+        [(null? xs)(error "list-nth-mod: empty list")]
+        [#t(let ([lstLength (length xs)])(list-ref xs (remainder n lstLength)))]
+        ))
