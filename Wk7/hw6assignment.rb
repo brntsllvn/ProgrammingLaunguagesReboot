7565fe45cc295c0308b7f41adf28d0bbd79246cb
Brent Sullivan <brntsllvn@gmail.com>
2017-02-25 21:40:31 -0800

    add first new piece and override super methods

--- Wk7/hw6assignment.rb
+++ Wk7/hw6assignment.rb
@@ -4,30 +4,48 @@
 # part of your solution.
 
 class MyPiece < Piece
-  # The constant All_My_Pieces should be declared here
-
+  
   # class array holding all the pieces and their rotations
-  All_My_Pieces = [[[[0, 0], [1, 0], [0, 1], [1, 1]]],  # square (only needs one)
-               rotations([[0, 0], [-1, 0], [1, 0], [0, -1]]), # T
-               [[[0, 0], [-1, 0], [1, 0], [2, 0]], # long (only needs two)
-               [[0, 0], [0, -1], [0, 1], [0, 2]]],
-               rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # L
-               rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]), # inverted L
-               rotations([[0, 0], [-1, 0], [0, -1], [1, -1]]), # S
-               rotations([[0, 0], [1, 0], [0, -1], [-1, -1]]), # Z
-							 # new piece 1
-							 # new piece 2
+  All_My_Pieces = [
+               #[[[0, 0], [1, 0], [0, 1], [1, 1]]],  # square (only needs one)
+               #rotations([[0, 0], [-1, 0], [1, 0], [0, -1]]), # T
+               #[[[0, 0], [-1, 0], [1, 0], [2, 0]], # long (only needs two)
+               #[[0, 0], [0, -1], [0, 1], [0, 2]]],
+               #rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # L
+               #rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]), # inverted L
+               #rotations([[0, 0], [-1, 0], [0, -1], [1, -1]]), # S
+               #rotations([[0, 0], [1, 0], [0, -1], [-1, -1]]), # Z
+               rotations([[0, 0], [-1, 0], [0, -1], [1, -1], [-1, -1]]) # enhanced S
+               # new piece 2
 							 # new piece 3
 					]
+
+  def self.next_piece (board)
+    MyPiece.new(All_My_Pieces.sample, board)
+  end
+
 end
 
 class MyBoard < Board
   
+  def initialize (game)
+    @grid = Array.new(num_rows) {Array.new(num_columns)}
+    @current_block = MyPiece.next_piece(self)
+    @score = 0
+    @game = game
+    @delay = 500
+  end
+
   def rotate_180
     if !game_over? and @game.is_running?
       @current_block.move(0, 0, 2)
     end
     draw
+  end
+
+  def next_piece
+    @current_block = MyPiece.next_piece(self)
+    @current_pos = nil
   end
 
 end
