e2c547eb5c0bb330cc184ea77bcacf69e5bfb41f
Brent Sullivan <brntsllvn@gmail.com>
2017-02-25 16:39:58 -0800

    add hw 6 stuff

---
+++ Wk7/hw6assignment.rb
@@ -0,0 +1,44 @@
+# University of Washington, Programming Languages, Homework 6, hw6runner.rb
+
+# This is the only file you turn in, so do not modify the other files as
+# part of your solution.
+
+class MyPiece < Piece
+  # The constant All_My_Pieces should be declared here
+
+  # class array holding all the pieces and their rotations
+  All_My_Pieces = [[[[0, 0], [1, 0], [0, 1], [1, 1]]],  # square (only needs one)
+               rotations([[0, 0], [-1, 0], [1, 0], [0, -1]]), # T
+               [[[0, 0], [-1, 0], [1, 0], [2, 0]], # long (only needs two)
+               [[0, 0], [0, -1], [0, 1], [0, 2]]],
+               rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # L
+               rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]), # inverted L
+               rotations([[0, 0], [-1, 0], [0, -1], [1, -1]]), # S
+               rotations([[0, 0], [1, 0], [0, -1], [-1, -1]]), # Z
+							 # new piece 1
+							 # new piece 2
+							 # new piece 3
+					]
+
+
+  # your enhancements here
+  # need to add 3 additional pieces
+  ## determine point arrays that describes each piece
+  ## confirm rotations function still works (it should?), or enhance
+  ## everything else should just work, right...?
+
+  
+
+
+end
+
+class MyBoard < Board
+  # your enhancements here
+
+end
+
+class MyTetris < Tetris
+  # your enhancements here
+
+end
+
