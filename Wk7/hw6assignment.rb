896c244a99ca8540effb536bf2ecfad21f7a2583
Brent Sullivan <brntsllvn@gmail.com>
2017-02-25 21:21:12 -0800

    180 degree enhancement refactor

--- Wk7/hw6assignment.rb
+++ Wk7/hw6assignment.rb
@@ -22,14 +22,14 @@ class MyPiece < Piece
 end
 
 class MyBoard < Board
-  # your enhancements here
-
+  
   def rotate_180
     if !game_over? and @game.is_running?
       @current_block.move(0, 0, 2)
     end
     draw
   end
+
 end
 
 class MyTetris < Tetris
@@ -52,28 +52,9 @@ class MyTetris < Tetris
     @board.draw
   end
 
-  def key_bindings  
-    @root.bind('n', proc {self.new_game}) 
-
-    @root.bind('p', proc {self.pause}) 
-
-    @root.bind('q', proc {exitProgram})
-    
-    @root.bind('a', proc {@board.move_left})
-    @root.bind('Left', proc {@board.move_left}) 
-    
-    @root.bind('d', proc {@board.move_right})
-    @root.bind('Right', proc {@board.move_right}) 
-
-    @root.bind('s', proc {@board.rotate_clockwise})
-    @root.bind('Down', proc {@board.rotate_clockwise})
-
-    @root.bind('w', proc {@board.rotate_counter_clockwise})
-    @root.bind('Up', proc {@board.rotate_counter_clockwise}) 
-    
+  def key_bindings
 		@root.bind('u', proc {@board.rotate_180}) 
-
-    @root.bind('space' , proc {@board.drop_all_the_way}) 
+    super
   end
 
 end
